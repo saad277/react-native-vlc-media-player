8e594f2fdee4279216c41bba044db789e2838da0
Saad <saadsheikh27@gmail.com>
2022-12-04 22:38:34 +0500

    removed react-native-slider

--- playerView/ControlBtn.js
+++ playerView/ControlBtn.js
@@ -1,7 +1,7 @@
 /**
  * Created by yuanzhou.xu on 2018/5/16.
  */
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
@@ -10,40 +10,39 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   StatusBar,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-// import Slider from 'react-native-slider';
-import PropTypes from 'prop-types';
-import TimeLimt from './TimeLimit';
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import PropTypes from "prop-types";
+import TimeLimt from "./TimeLimit";
 
 export default class ControlBtn extends Component {
   static defaultProps = {
-    titleGolive: 'Go live',
+    titleGolive: "Go live",
     showLeftButton: true,
     showMiddleButton: true,
-    showRightButton: true
-  }
+    showRightButton: true,
+  };
 
   _getTime = (data = 0) => {
     let hourCourse = Math.floor(data / 3600);
     let diffCourse = data % 3600;
     let minCourse = Math.floor(diffCourse / 60);
     let secondCourse = Math.floor(diffCourse % 60);
-    let courseReal = '';
+    let courseReal = "";
     if (hourCourse) {
       if (hourCourse < 10) {
-        courseReal += '0' + hourCourse + ':';
+        courseReal += "0" + hourCourse + ":";
       } else {
-        courseReal += hourCourse + ':';
+        courseReal += hourCourse + ":";
       }
     }
     if (minCourse < 10) {
-      courseReal += '0' + minCourse + ':';
+      courseReal += "0" + minCourse + ":";
     } else {
-      courseReal += minCourse + ':';
+      courseReal += minCourse + ":";
     }
     if (secondCourse < 10) {
-      courseReal += '0' + secondCourse;
+      courseReal += "0" + secondCourse;
     } else {
       courseReal += secondCourse;
     }
@@ -72,7 +71,7 @@ export default class ControlBtn extends Component {
       showLeftButton,
       showMiddleButton,
       showRightButton,
-      style
+      style,
     } = this.props;
     return (
       <View style={[styles.controls, style]}>
@@ -80,34 +79,45 @@ export default class ControlBtn extends Component {
           <TouchableOpacity style={styles.controlContent} activeOpacity={1}>
             <View style={styles.controlContent2}>
               <View style={styles.right}>
-                {
-                  showLeftButton ? (
-                    <TouchableOpacity
-                      activeOpacity={1}
-                      onPress={() => {
-                        onReplayPress && onReplayPress();
-                      }}
-                      style={{ width: 50, alignItems: 'center', justifyContent: 'center' }}>
-                      <Icon name={'replay'} size={30} color="#fff" />
-                    </TouchableOpacity>
-                  ) : <View style={{ width: 50 }} />
-                }
-                <Text
-                  style={{ fontSize: 11, color: '#fff' }}>       </Text>
-              </View>
-
-              {
-                showMiddleButton && (
+                {showLeftButton ? (
                   <TouchableOpacity
                     activeOpacity={1}
                     onPress={() => {
-                      onPausedPress && onPausedPress(!paused);
+                      onReplayPress && onReplayPress();
                     }}
-                    style={{ width: 50, alignItems: 'center', justifyContent: 'center' }}>
-                    <Icon name={paused ? 'play' : 'pause'} size={30} color="#fff" />
+                    style={{
+                      width: 50,
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Icon name={"replay"} size={30} color="#fff" />
                   </TouchableOpacity>
-                )
-              }
+                ) : (
+                  <View style={{ width: 50 }} />
+                )}
+                <Text style={{ fontSize: 11, color: "#fff" }}> </Text>
+              </View>
+
+              {showMiddleButton && (
+                <TouchableOpacity
+                  activeOpacity={1}
+                  onPress={() => {
+                    onPausedPress && onPausedPress(!paused);
+                  }}
+                  style={{
+                    width: 50,
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Icon
+                    name={paused ? "play" : "pause"}
+                    size={30}
+                    color="#fff"
+                  />
+                </TouchableOpacity>
+              )}
 
               {/* {showSlider && totalTime > 0 &&(
                 <View
@@ -152,24 +162,34 @@ export default class ControlBtn extends Component {
                   activeOpacity={1}
                   onPress={() => {
                     onGoLivePress && onGoLivePress();
-                  }}>
-                  <Text
-                    style={{ fontSize: 11, color: '#fff' }}>{showGoLive ? titleGolive : '       '}</Text>
+                  }}
+                >
+                  <Text style={{ fontSize: 11, color: "#fff" }}>
+                    {showGoLive ? titleGolive : "       "}
+                  </Text>
                 </TouchableOpacity>
-                {
-                  showRightButton ? (
-                    <TouchableOpacity
-                      activeOpacity={1}
-                      onPress={() => {
-                        onFullPress && onFullPress(!isFull);
-                      }}
-                      style={{ width: 50, alignItems: 'center', justifyContent: 'center' }}>
-                      <Icon name={isFull ? 'fullscreen-exit' : 'fullscreen'} size={30} color="#fff" />
-                    </TouchableOpacity>
-                  ) : <View style={{ width: 50 }} />
-                }
+                {showRightButton ? (
+                  <TouchableOpacity
+                    activeOpacity={1}
+                    onPress={() => {
+                      onFullPress && onFullPress(!isFull);
+                    }}
+                    style={{
+                      width: 50,
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Icon
+                      name={isFull ? "fullscreen-exit" : "fullscreen"}
+                      size={30}
+                      color="#fff"
+                    />
+                  </TouchableOpacity>
+                ) : (
+                  <View style={{ width: 50 }} />
+                )}
               </View>
-
             </View>
           </TouchableOpacity>
         </View>
@@ -184,89 +204,89 @@ const styles = StyleSheet.create({
     //backgroundColor: '#000',
   },
   controls: {
-    width: '100%',
+    width: "100%",
     height: 50,
   },
   rateControl: {
     flex: 0,
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 10,
     marginLeft: 10,
     //backgroundColor: 'rgba(0,0,0,0.5)',
     width: 120,
     height: 30,
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    justifyContent: "space-around",
+    alignItems: "center",
     borderRadius: 10,
   },
   controlOption: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 13,
-    color: '#fff',
+    color: "#fff",
     width: 30,
     //lineHeight: 12,
   },
   controlContainer: {
     flex: 1,
     //padding: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   controlContent: {
-    width: '100%',
+    width: "100%",
     height: 50,
     //borderRadius: 10,
-    backgroundColor: 'rgba(255,255,255,0.1)',
+    backgroundColor: "rgba(255,255,255,0.1)",
   },
   controlContent2: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 
   progress: {
     flex: 1,
     borderRadius: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   left: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   right: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   thumb: {
     width: 6,
     height: 18,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 4,
   },
   loading: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     top: 0,
     zIndex: 0,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   GG: {
-    backgroundColor: 'rgba(255,255,255,1)',
+    backgroundColor: "rgba(255,255,255,1)",
     height: 30,
     paddingLeft: 10,
     paddingRight: 10,
     borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
